96e0ebc9e4ed88931338e72378c9cf4b2bba5d88
StarFreedomX <starfreedomx@outlook.com>
2026-01-20 22:36:15 +0800

    fix: 图标采用相对路径

--- src/ktvServer.ts
+++ src/ktvServer.ts
@@ -328,7 +328,7 @@ export function runKTVServer(staticDir: string, redisUrl?: string) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>进入 KTV 房间</title>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
         <script src="https://cdn.tailwindcss.com"></script>
         <style>
             @keyframes slideUp {
