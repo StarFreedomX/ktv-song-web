af56be05463320e3dcab7fdfc991ea36a00385fc
StarFreedomX <starfreedomx@outlook.com>
2026-01-24 01:14:10 +0800

    feat: 前端启动脚本

---
+++ frontend/start-frontend.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const app = express();
+
+app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:5823',
+    changeOrigin: true
+}));
+
+app.use('/static', createProxyMiddleware({
+    target: 'http://localhost:5823',
+    changeOrigin: true
+}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+app.listen(5526, () => {
+    console.log('frontend server running at: http://localhost:5526');
+});
